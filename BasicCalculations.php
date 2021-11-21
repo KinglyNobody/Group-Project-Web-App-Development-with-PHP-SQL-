f17a210f2f05dfab4a633c1e28f710bb141fde39
Bea Muhonen <66080241+BeaJM@users.noreply.github.com>
2021-11-21 16:30:07 +0200

    Ekat

---
+++ BasicCalculations.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="MathTestStyles.css">
+	<body>
+<html>
+	<head>
+	<title>Math Test Final</title>
+	</head>
+<? php
+
+?>
+<h1> Basic Calculations 10 points </h1>
+		
+		<form> <!-- Tähän katsotaan myöhemmin W3Schools ohjeita https://www.w3schools.com/php/php_forms.asp -->
+			<label><b>1.</b> 98 - 56 + 45 =</label> 
+			<input type="text" id="question1" name="question1"> <br><br>
+			
+			<label><b>2.</b> 376 - 678 + 236  =</label> 
+			<input type="text" id="question2" name="question2"> <br><br>
+			
+			<label><b>3.</b> 6 X 7-9 X 5 =</label> 
+			<input type="text" id="question3" name="question3"> <br><br>
+			
+			<label><b>4.</b> 6 X 7-9 X 5 =</label> 
+			<input type="text" id="question4" name="question4"> <br><br>
+			
+			<label><b>5.</b> 5.6 X 34 + 21 / 7 =</label> 
+			<input type="text" id="question5" name="question5"> <br><br>
+			
+			<label><b>6.</b> 123.45 X 5.5 =</label> 
+			<input type="text" id="question6" name="question6"> <br><br>
+			
+			<label><b>7.</b> 3276.45 / 8  =</label> 
+			<input type="text" id="question7" name="question7"> <br><br>
+			
+			<label><b>8.</b> 748.5 / 1.5 =</label> 
+			<input type="text" id="question8" name="question8"> <br><br>
+			
+			<label><b>9.</b> 45600 / 100 =</label> 
+			<input type="text" id="question9" name="question9"> <br><br>
+			
+			<label><b>10.</b> 8763 X 100 =</label> 
+			<input type="text" id="question10" name="question10"> <br><br>
+			
+			<input type="submit" value="Next page">
+		</form>
+		
+	</body>
+</html>
---
+++ Units.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="MathTestStyles.css">
+	<body>
+<html>
+	<head>
+	<title>Math Test Final</title>
+	</head>
+<? php
+
+?>
+<h1> Units 20 Points </h1>
+		
+		<form> <!-- Tähän katsotaan myöhemmin W3Schools ohjeita https://www.w3schools.com/php/php_forms.asp -->
+		
+			<p> Change to milligrams </p>
+			
+			<label><b>1.</b> 925 micrograms =</label> 
+			<input type="text" id="mg1" name="mg1"> <br><br>
+			
+			<label><b>2.</b> 200 micrograms  =</label> 
+			<input type="text" id="mg2" name="mg2"> <br><br>
+			
+			<label><b>3.</b> 1386 micrograms =</label> 
+			<input type="text" id="mg3" name="mg3"> <br><br>
+			
+			<label><b>4.</b> 500 micrograms =</label> 
+			<input type="text" id="mg4" name="mg4"> <br><br>
+			
+			<p> Change to grams </p>
+			
+			<label><b>1.</b> 7260 mg =</label> 
+			<input type="text" id="grams1" name="grams1"> <br><br>
+			
+			<label><b>2.</b> 80 mg =</label> 
+			<input type="text" id="grams2" name="qrams2"> <br><br>
+			
+			<label><b>3.</b> 135 mg  =</label> 
+			<input type="text" id="grams3" name="grams3"> <br><br>
+			
+			<label><b>4.</b> 1250 mg =</label> 
+			<input type="text" id="grams4" name="grams4"> <br><br>
+			
+			<p> Change to Milliliters </p>
+			
+			<label><b>1.</b> 4.5 L =</label> 
+			<input type="text" id="ml1" name="ml1"> <br><br>
+			
+			<label><b>2.</b> 0.9 L =</label> 
+			<input type="text" id="ml2" name="ml2"> <br><br>
+			
+			<label><b>3.</b> 8.5 L =</label> 
+			<input type="text" id="ml3" name="ml3"> <br><br>
+			
+			<label><b>4.</b> 2.2 L =</label> 
+			<input type="text" id="ml4" name="ml4"> <br><br>
+			
+			<p> Change to Liters </p>
+			
+			<label><b>1.</b>  70 ml =</label> 
+			<input type="text" id="l1" name="l1"> <br><br>
+			
+			<label><b>2.</b> 725 ml =</label> 
+			<input type="text" id="l2" name="l2"> <br><br>
+			
+			<label><b>3.</b> 1575 ml =</label> 
+			<input type="text" id="l3" name="l3"> <br><br>
+			
+			<label><b>4.</b> 3300 ml =</label> 
+			<input type="text" id="l4" name="l4"> <br><br>
+			
+			<p> Change to micrometer </p>
+			
+			<label><b>1.</b> 128 mm =</label> 
+			<input type="text" id="micrometer1" name="micrometer1"> <br><br>
+			
+			<label><b>2.</b>  32 mm =</label> 
+			<input type="text" id="micrometer2" name="micrometer2"> <br><br>
+			
+			<label><b>3.</b> 3.55 mm =</label> 
+			<input type="text" id="micrometer3" name="micrometer3"> <br><br>
+			
+			<label><b>4.</b> 22.45 mm =</label> 
+			<input type="text" id="micrometer4" name="micrometer4"> <br><br>
+			
+			<input type="submit" value="Submit">
+		</form>
+		
+	</body>
+</html>
