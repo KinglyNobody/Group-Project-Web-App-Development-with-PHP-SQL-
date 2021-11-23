9357654e20829abdb0270dab2e9e65e2ec5b6166
KinglyNobody <77964619+KinglyNobody@users.noreply.github.com>
2021-11-23 14:50:48 +0200

    Changed html to php, Added Bootstrap and container for PHP-files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php 
+//PHP TÄNNE
+?>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./styles.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <div class="container">
+          <h1>Welcome to Test App</h1>
+        </div>
+    </body>
+</html>
