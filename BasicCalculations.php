38095beaa044b6821c4685dd0ef20803c1863623
Bea Muhonen <66080241+BeaJM@users.noreply.github.com>
2021-11-26 19:08:46 +0200

    Testing points

--- BasicCalculations.php
+++ BasicCalculations.php
@@ -3,11 +3,12 @@ include_once 'header.php';
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 $name = $_POST["name"];
 $studentid =  $_POST["studentid"];
+$tulos = $_POST["tulos"];
 ?>
 
 <h1> Basic Calculations 10 points </h1>
 		
-		<form method="post" action="Units.php"> 
+		<form method="post" action="<?php echo $tulos?>;"> 
 			
 		<input type="hidden" name='name' value="<?php echo $name; ?>">
 		<input type="hidden" name='studentid' value="<?php echo $studentid; ?>">
@@ -16,110 +17,90 @@ $studentid =  $_POST["studentid"];
 			<input type="text" id="question1" name="question1" value="<?php $question1; ?>"> <br><br>
 			
 			<?php 
-			if ($question1 == 87) {
+			if ($question1 == "87") {
 			    $tulos = 1;
-			} else {
-			    $tulos = 0;
-			}    
+			}
 			?>	
 			
 			<label><b>2.</b> 376 - 678 + 236  =</label> 
 			<input type="text" id="question2" name="question2" value="<?php $question2; ?>"> <br><br>
 			
 			<?php 
-			if ($question2 == -66) {
+			if ($question2 == "-66") {
 			    $tulos + 1;
-			} else {
-			    $tulos + 0;
-			}    
+			} 
 			?>	
 			
 			<label><b>3.</b> 6 X 7-9 X 5 =</label> 
 			<input type="text" id="question3" name="question3" value="<?php $question3; ?>"> <br><br>
 			
 			<?php 
-			if ($question3 == -3) {
+			if ($question3 == "-3") {
 			    $tulos + 1;
-			} else {
-			    $tulos + 0;
-			}    
+			}
 			?>
 			
 			<label><b>4.</b> 6 X 7-9 X 5 =</label> 
 			<input type="text" id="question4" name="question4" value="<?php $question4; ?>"> <br><br>
 			
 			<?php 
-			if ($question4 == -80) {
+			if ($question4 == "-80") {
 			    $tulos + 1;
-			} else {
-			    $tulos + 0;
-			}    
+			}
 			?>
 			
 			<label><b>5.</b> 5.6 X 34 + 21 / 7 =</label> 
 			<input type="text" id="question5" name="question5" value="<?php $question5; ?>"> <br><br>
 			
 			<?php 
-			if ($question5 == 193.4) {
+			if ($question5 == "193.4" or $question5 == "193,4" ) {
 			    $tulos + 1;
-			} else {
-			    $tulos + 0;
-			}    
+			} 
 			?>
 			
 			<label><b>6.</b> 123.45 X 5.5 =</label> 
 			<input type="text" id="question6" name="question6" value="<?php $question6; ?>"> <br><br>
 			
 			<?php 
-			if ($question6 == 678.975) {
+			if ($question6 == "678.975" or $question6 == "678,975" ) {
 			    $tulos + 1;
-			} else {
-			    $tulos + 0;
-			}    
+			} 
 			?>
 			
 			<label><b>7.</b> 3276.45 / 8  =</label> 
 			<input type="text" id="question7" name="question7" value="<?php $question7; ?>"> <br><br>
 			
 			<?php 
-			if ($question7 == 409.55) {
+			if ($question7 == "409.55" or $guestion7 == "409,55") {
 			    $tulos + 1;
-			} else {
-			    $tulos + 0;
-			}    
+			} 
 			?>
 			
 			<label><b>8.</b> 748.5 / 1.5 =</label> 
 			<input type="text" id="question8" name="question8" value="<?php $question8; ?>"> <br><br>
 			
 			<?php 
-			if ($question8 == 499) {
+			if ($question8 == "499") {
 			    $tulos + 1;
-			} else {
-			    $tulos + 0;
-			}    
+			}  
 			?>
 			
 			<label><b>9.</b> 45600 / 100 =</label> 
 			<input type="text" id="question9" name="question9" value="<?php $question9; ?>"> <br><br>
 			
 			<?php 
-			if ($question9 == 456) {
+			if ($question9 == "456") {
 			    $tulos + 1;
-			} else {
-			    $tulos + 0;
-			}    
+			}   
 			?>
 			
 			<label><b>10.</b> 8763 X 100 =</label> 
 			<input type="text" id="question10" name="question10" value="<?php $question10; ?>"> <br><br>
 			
 			<?php 
-			if ($question10 == 876300) {
+			if ($question10 == "876300") {
 			    $tulos + 1;
-			} else {
-			    $tulos + 0;
-			}    
+			}   
 			?>
 			
 			<input type="submit" value="Submit Answers">
