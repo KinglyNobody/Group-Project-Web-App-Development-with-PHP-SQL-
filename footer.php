f14e6da5c1c7d6cd9cd8e2fc5f04b8189e0c742e
KinglyNobody <77964619+KinglyNobody@users.noreply.github.com>
2021-11-28 10:30:00 +0200

    removed comments

--- footer.php
+++ footer.php
@@ -20,26 +20,20 @@
 					?>
 		
 						<script>
-						// Set the date we're counting down to
 						var countDownDate = <?php echo strtotime($time) ?> * 1000;
 						var now = <?php echo time() ?> * 1000;
 
-						// Update the count down every 1 second
 						var x = setInterval(function() {
 							
 							now = now + 1000
 							
-							// Find the distance between now and the count down date
 							var distance = countDownDate - now;
 								
-							// Time calculations for days, hours, minutes and seconds
 							var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 							var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 								
-							// Output the result in an element with id="timer"
 							document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
 								
-							// If the count down is over, write some text 
 							if (distance < 0) {
 								clearInterval(x);
 							document.getElementById("tuner").innerHTML = "EXPIRED";
